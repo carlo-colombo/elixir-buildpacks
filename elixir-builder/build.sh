4b74fadf8b65f3d730df4c4b2d797bf24627860d
Carlo Colombo <carlo.colombo@gmx.com>
2021-01-12 17:59:35 +0100

    downloading erlang and elixir outside of the dockerfile

--- elixir-builder/build.sh
+++ elixir-builder/build.sh
@@ -1,6 +1,20 @@
 #! /bin/bash
 set -eo pipefail
 
+[ -z "$DEBUG" ] || set -x
+
+ERLANG_VERSION="23.1-1"
+ELIXIR_VERSION="v1.11.2"
+
+set +e
+wget "https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_${ERLANG_VERSION}~ubuntu~bionic_amd64.deb" .\
+    -O ./packages/erlang \
+    --no-clobber
+wget "https://github.com/elixir-lang/elixir/releases/download/${ELIXIR_VERSION}/Precompiled.zip" \
+    -O ./packages/elixir \
+    --no-clobber
+set -e
+
 for tag in run build
 do
     docker build . -f "Dockerfile.$tag" -t "rg.nl-ams.scw.cloud/carlo-colombo/elixir-builder:$tag"
